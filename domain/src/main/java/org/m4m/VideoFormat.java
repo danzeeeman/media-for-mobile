c39c78a2b153c789f3946f7e7e1d941f9d1d56f8
Dan Moore <danzeeeman@users.noreply.github.com>
2017-09-19 15:18:29 -0400

    Update VideoFormat.java

--- domain/src/main/java/org/m4m/VideoFormat.java
+++ domain/src/main/java/org/m4m/VideoFormat.java
@@ -91,9 +91,6 @@ public abstract class VideoFormat extends MediaFormat {
      * @param bitRate Video bit rate in KBytes.
      */
     public void setVideoBitRateInKBytes(int bitRate) {
-        if (width * height * 30 * 2 * 0.00007 < bitRate) {
-            bitRate = (int) (width * height * 30 * 2 * 0.00007);
-        }
         setInteger(KEY_BIT_RATE, bitRate * 1024);
     }
 
